637a78a77decc3fba42b6c55286ca95cfcb49dba
gtd9511 <ydj02109@naver.com>
2022-11-11 14:15:30 +0900

    221111 s01

---
+++ push_swap/make_push_swap/includes/push_swap.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanghan <sanghan@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/11 01:06:57 by sanghan           #+#    #+#             */
+/*   Updated: 2022/11/11 13:42:52 by sanghan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include "../libft/libft.h"
+# include "../libft/get_next_line.h"
+
+
+#endif
