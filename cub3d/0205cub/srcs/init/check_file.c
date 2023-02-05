9c2662d920183ba4921633f25413c72ab8c6a327
gtd9511 <ydj02109@naver.com>
2023-02-05 20:23:06 +0900

    230205 c00 keypress

---
+++ cub3d/0205cub/srcs/init/check_file.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_file.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: doykim <doykim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/17 16:19:27 by doykim            #+#    #+#             */
+/*   Updated: 2023/01/19 19:18:03 by doykim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	check_file(int ac, char **av, t_game *game)
+{
+	char	buff[1024];
+	int		fd;
+
+	check_argument(ac, av);
+	fd = open(av[1], O_RDONLY);
+	if (fd == -1)
+		error_exit(1);
+	read(fd, buff, 1024);
+	init_element(buff, game);
+	close(fd);
+}
+
+void	check_argument(int ac, char **av)
+{
+	int		i;
+	int		len;
+
+	if (ac != 2)
+		error_exit(1);
+	len = ft_strlen(av[1]);
+	i = len - 4;
+	if (i <= 5)
+		error_exit(1);
+	if (!ft_strnstr(av[1], "maps/", 5))
+		error_exit(1);
+	if (!ft_strnstr(av[1] + i, ".cub\0", 5))
+		error_exit(1);
+}
