936696563ad88ba0f6198731917e4a871a385cda
gtd9511 <ydj02109@naver.com>
2023-03-15 20:42:56 +0900

    230315 c01 cpp03/start

--- CPP_Module/CPP_Module_03/ex00/includes/ClapTrap.hpp
+++ CPP_Module/CPP_Module_03/ex00/includes/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: sanghan <sanghan@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 10:27:29 by sanghan           #+#    #+#             */
-/*   Updated: 2023/03/15 10:52:22 by sanghan          ###   ########.fr       */
+/*   Updated: 2023/03/15 19:43:51 by sanghan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,23 @@
 # define CLAPTRAP_HPP
 
 #include <iostream>
+
 class ClapTrap
 {
 private:
-	/* data */
-	//name, hit points, energy points, attack damage
+	std::string	_name;
+	unsigned int	_hitpoint;
+	unsigned int	_energypoint;
+	unsigned int	_attackdamage;
+
 public:
-	ClapTrap(/* args*/);
-	~ClapTrap();
+	ClapTrap(void);
+	~ClapTrap(void);
+	ClapTrap(std::string name);
+	ClapTrap(const ClapTrap& obj);
+	ClapTrap& operator=(const ClapTrap& obj);
+
+	//unsigned int getDamage(void) const;
 
 	void	attack(const std::string& target);
 	void	takeDamage(unsigned int amount);
--- CPP_Module/CPP_Module_03/ex00/sources/ClapTrap.cpp
+++ CPP_Module/CPP_Module_03/ex00/sources/ClapTrap.cpp
@@ -6,7 +6,45 @@
 /*   By: sanghan <sanghan@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 10:27:28 by sanghan           #+#    #+#             */
-/*   Updated: 2023/03/15 10:27:29 by sanghan          ###   ########.fr       */
+/*   Updated: 2023/03/15 20:33:33 by sanghan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/ClapTrap.hpp"
+
+ClapTrap::ClapTrap(void): _name("default"), _hitpoint(10), _energypoint(10), _attackdamage(0)
+{
+	std::cout << "ClapTrap " << this->_name << " is constructed" << std::endl;
+}
+
+ClapTrap::~ClapTrap(void)
+{
+	std::cout << "ClapTrap " << this->_name << "is deconstructed" << std::endl;
+}
+
+ClapTrap::ClapTrap(std::string name): _name(name), _hitpoint(10), _energypoint(10), _attackdamage(0)
+{
+	std::cout << "ClapTrap " << this->_name << " is constructed" << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap& obj): _name(obj._name), _hitpoint(obj._hitpoint), _energypoint(obj._energypoint), _attackdamage(obj._attackdamage)
+{
+	std::cout << "ClapTrap " << this->_name << " is copied" << std::endl;
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap& obj)
+{
+	this->_name = obj._name;
+
+
+}
+
+//unsigned int getDamage(void) const;
+void	ClapTrap::attack(const std::string& target)
+{}
+
+void	ClapTrap::takeDamage(unsigned int amount)
+{}
+
+void	ClapTrap::beRepaired(unsigned int amount)
+{}
