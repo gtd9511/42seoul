03d92d59196929794ade64d25da9f579d3d3652a
gtd9511 <ydj02109@naver.com>
2023-04-03 20:00:23 +0900

    230403 c00 CPP06 Clear

--- CPP_Module/CPP_Module_06/ex00/includes/ScalarConverter.hpp
+++ CPP_Module/CPP_Module_06/ex00/includes/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: sanghan <sanghan@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 08:40:47 by sanghan           #+#    #+#             */
-/*   Updated: 2023/03/30 21:29:24 by sanghan          ###   ########.fr       */
+/*   Updated: 2023/04/03 16:31:53 by sanghan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,14 @@
 
 #include <iostream>
 #include <string>
-//#include <iomanip>
-//#include <cmath>
+#include <iomanip>
+#include <limits>
 
 class ScalarConverter
 {
 	public:
 		static void convert(const std::string input);
+		static int	checkType(const std::string& input);
 };
 
 #endif
--- CPP_Module/CPP_Module_06/ex00/sources/ScalarConverter.cpp
+++ CPP_Module/CPP_Module_06/ex00/sources/ScalarConverter.cpp
@@ -6,42 +6,101 @@
 /*   By: sanghan <sanghan@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 08:40:46 by sanghan           #+#    #+#             */
-/*   Updated: 2023/04/02 21:00:46 by sanghan          ###   ########.fr       */
+/*   Updated: 2023/04/03 17:20:04 by sanghan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ScalarConverter.hpp"
 
-void ScalarConverter::convert(const std::string input)
+void	errorMessage(int n)
 {
-	double	dvalue;
-	float	fvalue;
-	int	ivalue;
-
-	char *ptr = NULL;
-	dvalue = strtod(input.c_str(), &ptr);
-
-
-
-	std::cout << "char: ";
-	if (ivalue > 31 && ivalue < 127)
-		std::cout << static_cast<char>(ivalue) << std::endl;
-	else if ((ivalue >= 0 && ivalue < 32) || ivalue == 127)
-		std::cout << "Non displayable" << std::endl;
-	else
-		std::cout << "impossible" << std::endl;
-
-	std::cout << "int: " << std::endl;
-	if (dvalue < 2147483648 && dvalue > -2147483649)
-		std::cout << static_cast<int>(dvalue) << std::endl;
-	else
-	 	std::cout << "impossible" << std::endl;
-
-	std::cout << "float: " << std::endl;
-
-	std::cout << "double: " << std::endl;
+	if (n == 0)
+		std::cout << "char: impossible" << std::endl;
+	if (n == 1)
+		std::cout << "int: impossible" << std::endl;
+	if (n == 2)
+		std::cout << "float: nanf" << std::endl;
+	if (n == 3)
+		std::cout << "double: nan" << std::endl;
+	return ;
 }
 
-//a~z
+int	ScalarConverter::checkType(const std::string &input)
+{
+	char *ptr = NULL;
+	std::strtod(input.c_str(), &ptr);
+	if (input == "nanf" || input == "+inff" || input == "-inff" || input == "nan" || input == "+inf" || input == "-inf")
+		return (0);
+	if (ptr[0] == '\0' || (std::string(ptr) == "f" && input.size() != 1))
+		return (1);
+	else if (input.size() == 1 && std::isprint(input[0]))
+		return (2);
+	return (3);
+}
 
-//0~127
+void ScalarConverter::convert(const std::string input)
+{
+	double	value;
+
+	char *ptr = NULL;
+	value = strtod(input.c_str(), &ptr);
+
+	if (checkType(input) == 3)
+	{
+		for (int i = 0; i < 4; i++)
+			errorMessage(i);
+	}
+	if (checkType(input) == 0)
+	{
+		for (int i = 0; i < 2; i++)
+			errorMessage(i);
+		if (input == "nanf" || input == "nan")
+		{
+			std::cout << "float: nanf" << std::endl;
+			std::cout << "double: nan" << std::endl;
+		}
+		else if (input == "+inff" || input == "+inf")
+		{
+			std::cout << "float: +inff" << std::endl;
+			std::cout << "double: +inf" << std::endl;
+		}
+		else if (input == "-inff" || input == "-inf")
+		{
+			std::cout << "float: -inff" << std::endl;
+			std::cout << "double: -inf" << std::endl;
+		}
+	}
+
+	if (checkType(input) == 2)
+	{
+		std::cout << "char: " << input[0] << std::endl;
+		std::cout << "int: " << static_cast<int>(input[0]) << std::endl;
+		std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(input[0]) << "f" << std::endl;
+		std::cout << "double: " << std::setprecision(1) << static_cast<double>(input[0]) << std::endl;
+
+	}
+
+	if (checkType(input) == 1)
+	{
+		if (value > 31 && value < 127)
+			std::cout << "char: " << static_cast<char>(value) << std::endl;
+		else if ((value >= 0 && value < 32) || (value >= 127 && value < 128))
+			std::cout << "char: Non displayable" << std::endl;
+		else
+			errorMessage(0);
+		if (value < 2147483648 && value > -2147483649)
+			std::cout << "int: " << static_cast<int>(value) << std::endl;
+		else
+			errorMessage(1);
+		std::cout << "float: ";
+		if (static_cast<float>(value) == static_cast<int>(static_cast<float>(value)))
+			std::cout << static_cast<float>(value) << ".0f" << std::endl;
+		else
+		 	std::cout << std::setprecision(std::numeric_limits<float>::digits10) << static_cast<float>(value) << "f" << std::endl;
+		std::cout <<"double: ";
+		if (value == static_cast<int>(value))
+			std::cout << value << ".0" << std::endl;
+		else
+		 	std::cout << std::setprecision(std::numeric_limits<float>::digits10) << value << std::endl;
+	}
+}
