f4383c09fa41985b517b4ab8ed53aebf1f56af38
gtd9511 <ydj02109@naver.com>
2023-04-13 23:14:57 +0900

    220413 m01 CPP07

--- CPP_Module/CPP_Module_07/ex00/sources/main.cpp
+++ CPP_Module/CPP_Module_07/ex00/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sanghan <sanghan@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 21:37:50 by sanghan           #+#    #+#             */
-/*   Updated: 2023/04/13 16:29:30 by sanghan          ###   ########.fr       */
+/*   Updated: 2023/04/13 22:27:40 by sanghan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@ int main(void)
 	int a = 2;
 	int b = 3;
 
-	::swap( a, b );
+	::swap(a, b);
 	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+	std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
+	std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
 	return 0;
 }
--- CPP_Module/CPP_Module_07/ex01/sources/main.cpp
+++ CPP_Module/CPP_Module_07/ex01/sources/main.cpp
@@ -6,7 +6,29 @@
 /*   By: sanghan <sanghan@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 21:38:09 by sanghan           #+#    #+#             */
-/*   Updated: 2023/03/30 21:38:10 by sanghan          ###   ########.fr       */
+/*   Updated: 2023/04/13 22:31:17 by sanghan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "iter.hpp"
+
+int main() {
+  std::string s[6] = { "42", "Seoul", "cpp", "module", "07", "sanghan" };
+  int i[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+  double d[10] = { 0.0, 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9};
+  char c[12] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l' };
+
+  std::cout << "String Iteration : ";
+  ::iter(s, 6, ::print);
+  std::cout << std::endl;
+  std::cout << "Int Iteration : ";
+  ::iter(i, 10, ::print);
+  std::cout << std::endl;
+  std::cout << "Double Iteration : ";
+  ::iter(d, 10, ::print);
+  std::cout << std::endl;
+  std::cout << "Char Iteration : ";
+  ::iter(c, 12, ::print);
+  std::cout << std::endl;
+  return 0;
+}
