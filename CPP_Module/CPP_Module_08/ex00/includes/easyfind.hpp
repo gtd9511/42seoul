ff5e53f732aeda089f37ed89b23926cd8e2857b1
gtd9511 <ydj02109@naver.com>
2023-06-14 21:52:05 +0900

    230614 c00 CPP08 ex00

--- CPP_Module/CPP_Module_08/ex00/includes/easyfind.hpp
+++ CPP_Module/CPP_Module_08/ex00/includes/easyfind.hpp
@@ -6,24 +6,26 @@
 /*   By: sanghan <sanghan@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 21:43:11 by sanghan           #+#    #+#             */
-/*   Updated: 2023/04/14 13:02:51 by sanghan          ###   ########.fr       */
+/*   Updated: 2023/06/14 21:34:40 by sanghan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
-# include <iostream>
-# include <algorithm>
-# include <vector>
-# include <list>
+#include <iostream>
+#include <algorithm>
+#include <stdexcept>
+#include <vector>
+#include <list>
+#include <deque>
 
 template <typename T>
 typename T::iterator easyfind(T &container, int n)
 {
 	typename T::iterator it = std::find(container.begin(), container.end(), n);
 	if (it == container.end())
-		throw std::exception();
+		throw std::invalid_argument("Value does not exist.");
 	return it;
 }
 
--- CPP_Module/CPP_Module_08/ex00/sources/main.cpp
+++ CPP_Module/CPP_Module_08/ex00/sources/main.cpp
@@ -6,38 +6,36 @@
 /*   By: sanghan <sanghan@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 21:43:14 by sanghan           #+#    #+#             */
-/*   Updated: 2023/04/13 16:38:50 by sanghan          ###   ########.fr       */
+/*   Updated: 2023/06/14 21:40:57 by sanghan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <vector>
-#include <list>
-#include <algorithm>
 #include "../includes/easyfind.hpp"
 
 int main(void)
 {
 	std::vector<int> v;
 	std::list<int> l;
+	std::deque<int> d;
 	int i = 0;
 
 	while (i < 10)
 	{
 		v.push_back(i);
 		l.push_back(i);
+		d.push_back(i);
 		i++;
 	}
 	try
 	{
-		std::cout << easyfind(v, 5) << std::endl;
-		std::cout << easyfind(l, 5) << std::endl;
-		std::cout << easyfind(v, 10) << std::endl;
-		std::cout << easyfind(l, 10) << std::endl;
+		std::cout << *(easyfind(v, 3)) << std::endl;
+		std::cout << *(easyfind(l, 5)) << std::endl;
+		std::cout << *(easyfind(d, 7)) << std::endl;
+		std::cout << *(easyfind(d, 10)) << std::endl;
 	}
 	catch (std::exception &e)
 	{
-		std::cout << "Not found" << std::endl;
+		std::cout << e.what() << std::endl;
 	}
 	return 0;
 }
